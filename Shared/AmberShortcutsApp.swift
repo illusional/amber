025d8099df706a5a9d8855ffbaded4bcc56c5c51
Michael Franklin <michael@illusional.net>
2022-05-30 20:49:27 +1000

    Initial Commit

---
+++ Shared/AmberShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmberShortcutsApp.swift
+//  Shared
+//
+//  Created by Michael Franklin on 30/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct AmberShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
